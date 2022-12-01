4b54785364dd00409dcc9d9b139da541fd35d0ca
JoseRobertsVery <jose.r@verypossible.com>
2022-12-01 13:00:26 -0600

    refactored to follow more elixir like convention

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -15,13 +15,21 @@ defmodule Issues.CLI do
     `argv` is a list of strings that represent the command line arguments
     """
     def parse_args(argv) do
-        parse = OptionParser.parse(argv, switches: [help: :boolean],
-            aliases: [h: :help])
-        case parse do
-            {[ help: true], _, _} -> :help
-            { _, [user, project, count ], _} -> { user, project, String.to_integer(count)}
-            { _, [user, project], _} -> { user, project, @default_count}
-            _ -> :help
-        end
+        OptionParser.parse(argv, switches: [help: :boolean],
+                                 aliases: [h: :help])
+        |> elem(1)
+        |> args_to_internal_representation()
+    end
+
+    def args_to_internal_representation([user, project, count]) do
+        { user, project, String.to_integer(count) }
+    end
+
+    def args_to_internal_representation([user, project]) do
+        { user, project, @default_count }
+    end
+
+    def args_to_internal_representation(_) do
+        :help
     end
 end
