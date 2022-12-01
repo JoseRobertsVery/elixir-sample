d96c68fb249a856dfc6c085c786ba1de25c6281b
JoseRobertsVery <jose.r@verypossible.com>
2022-12-01 12:52:01 -0600

    add cli tests and credo deps

---
+++ lib/issues/cli.ex
@@ -0,0 +1,27 @@
+defmodule Issues.CLI do
+    @default_count 4
+
+    @moduledoc """
+    Handle the command line parsing and the dispatch to​
+    the various functions that end up generating a​
+    table of the last _n_ issues in a github project
+    """
+
+    def run(argv) do
+        parse_args(argv)
+    end
+
+    @doc """
+    `argv` is a list of strings that represent the command line arguments
+    """
+    def parse_args(argv) do
+        parse = OptionParser.parse(argv, switches: [help: :boolean],
+            aliases: [h: :help])
+        case parse do
+            {[ help: true], _, _} -> :help
+            { _, [user, project, count ], _} -> { user, project, String.to_integer(count)}
+            { _, [user, project], _} -> { user, project, @default_count}
+            _ -> :help
+        end
+    end
+end
